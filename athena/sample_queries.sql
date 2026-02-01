d4e12acdf10659956827f13f24d5ca0886c104ab
Ayshak532 <ayshak532532@gmail.com>
2026-02-01 15:35:18 +0530

    Update sample_queries.sql

--- athena/sample_queries.sql
+++ athena/sample_queries.sql
@@ -13,8 +13,3 @@ SELECT name, followers
 FROM spotify.datawarehouse
 ORDER BY followers DESC
 LIMIT 10;
-
-SELECT artist_name, followers
-FROM spotify.spotify_datawarehouse
-ORDER BY followers DESC
-LIMIT 10;
